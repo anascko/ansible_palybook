d5520aba898d6b1c458307eec44598725626af82
anascko <anascko@gmail.com>
2018-01-03 19:06:48 +0200

    Update json_inv.py

--- playbooks/inventory/json_inv.py
+++ playbooks/inventory/json_inv.py
@@ -6,40 +6,35 @@ import json
 def _prepare_inventory():
     hostvars = {}
     groups = {}
-#    groups.update({'slave': []})
-#    groups.update({'master': []})
     groups.update({'slave': {'hosts': []}})
     groups.update({'master': {'hosts': []}})
     return (groups, hostvars)
 
 def main():
-
-    vars={
-         'ansible_user': 'root',
-         'ansible_ssh_pass': 'r00tme'
-    }
+    varss = {
+        'ansible_user': 'root',
+        'ansible_ssh_pass': 'r00tme'
+            }
     (groups, hostvars) = _prepare_inventory()
-
+    group_name=''
+    host={}
     conn = libvirt.open("qemu:///system")
     lease=conn.networkLookupByName("default").DHCPLeases()
-    for inser in lease:
-        if inser['hostname'] in hostvars.keys():
-            newhost=inser['hostname'] +'-'+ inser['mac']
-            hostvars.update({newhost : {'ansible_ssh_host' : inser['ipaddr']}})
+    for row in lease:
+        host['name'] = row['hostname']
+        if row['hostname'] == 'jenkins':
+            group_name = 'master'
         else:
-            hostvars.update({inser['hostname']: {'ansible_ssh_host' :inser['ipaddr']}})
-        if inser['hostname'] != 'jenkins':
-            hosts=inser['hostname']
-#            print type(hosts)
-            groups.update({'slave': hosts})
-#            groups.update({'slave': inser['hostname']})
-        else:
-            groups.update({'master': inser['hostname']})
-    hostvars.update(vars)
-    inventory = {'_meta': {'hostvars': hostvars}}
+            group_name = 'slave'
+        groups[group_name]['hosts'].append(row['ipaddr'])
+#        groups[group_name]['hosts'].append(host['name'])
+#        hostvars.update({host['name']: row['ipaddr']})
+        hostvars.update({row['ipaddr']: host['name']})
+    inventory = {'_meta': {'hostsvars': hostvars, 'vars': varss}}
     inventory.update(groups)
-#    inventory.update(vars)
-    print(json.dumps(inventory, indent=2))
+#    inventory.update({'vars' : varss})
+#    print(json.dumps(inventory, indent=2))
+    print (inventory)
 
 if __name__ == '__main__':
     main()
