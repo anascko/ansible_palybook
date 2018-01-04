d3519c9f60dc9b41505b2afbf78cf9bccb5555fd
anascko <anascko@gmail.com>
2018-01-04 11:44:59 +0200

    Update json_inv.py

--- playbooks/inventory/json_inv.py
+++ playbooks/inventory/json_inv.py
@@ -6,15 +6,12 @@ import json
 def _prepare_inventory():
     hostvars = {}
     groups = {}
-    groups.update({'slave': {'hosts': []}})
-    groups.update({'master': {'hosts': []}})
+    groups.update({'slave': {'hosts': [],'vars': {'ansible_user': 'root', 'ansible_ssh_pass': 'r00tme'}}})
+    groups.update({'master': {'hosts': [], 'vars': {'ansible_user': 'root', 'ansible_ssh_pass': 'r00tme'}}})
     return (groups, hostvars)
 
+
 def main():
-    varss = {
-        'ansible_user': 'root',
-        'ansible_ssh_pass': 'r00tme'
-            }
     (groups, hostvars) = _prepare_inventory()
     group_name=''
     host={}
@@ -27,14 +24,10 @@ def main():
         else:
             group_name = 'slave'
         groups[group_name]['hosts'].append(row['ipaddr'])
-#        groups[group_name]['hosts'].append(host['name'])
-#        hostvars.update({host['name']: row['ipaddr']})
         hostvars.update({row['ipaddr']: host['name']})
-    inventory = {'_meta': {'hostsvars': hostvars, 'vars': varss}}
+    inventory = {'_meta': {'hostsvars': hostvars}}
     inventory.update(groups)
-#    inventory.update({'vars' : varss})
-#    print(json.dumps(inventory, indent=2))
-    print (inventory)
+    print json.dumps(inventory, indent=2)
 
 if __name__ == '__main__':
     main()
